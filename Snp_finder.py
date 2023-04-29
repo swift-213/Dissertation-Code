187a504ee0f87af7afac12fa9117d7dedc0c2f85
swift-213 <106386456+swift-213@users.noreply.github.com>
2023-04-29 14:58:08 +0100

    Adding the optional contigs flag

--- Snp_finder.py
+++ Snp_finder.py
@@ -10,7 +10,7 @@ parser = ArgumentParser()
 parser.add_argument("-i", "--input", help="Add in the VCF file under this flag", required=True)
 parser.add_argument("-i2", "--input2", help="Add in the VCF file under this flag", required=False)
 parser.add_argument("-o", "--output", action='store', help="Assembly information, SNP, INDELs, matches, number of lines", required=False)
-
+parser.add_argument("--Use_specific_Contigs", action='store_true', help="Use Specific contigs in -i2 textfile format with each contig on a new line", required=False)
 args = parser.parse_args()
 
 
@@ -23,11 +23,12 @@ output = args.output
 
 
 #Reading in the chrom_file and turning it into a table 
-file1 = open(chrom_file, 'r')
-Lines = file1.readlines()
-chrom=[]
-for line in Lines:
-    chrom.append("{}".format(line.strip()))
+if args.input2 and args.Use_specific_Contigs:
+    file1 = open(chrom_file, 'r')
+    Lines = file1.readlines()
+    chrom=[]
+    for line in Lines:
+        chrom.append("{}".format(line.strip()))
 
 # fetch the VCF columns as variants
 all_variants = vcf.fetch()
@@ -38,19 +39,28 @@ Assembly_info = dict.fromkeys(['number_of_lines', 'matches', 'mismatches'], 0)
 previous = -1
 
 
-
-for variant in variants:
-    if variant.chrom in chrom:
-        #Get the number of lines
+if args.input2 and args.Use_specific_Contigs:
+    for variant in variants:
+        if variant.chrom in chrom:
+            #Get the number of lines
+            if variant.pos != previous:
+                Assembly_info['number_of_lines'] += 1
+            #Get the number of matches
+            if variant.alts == None and variant.info['DP'] != 0:
+                Assembly_info['matches'] += 1
+            if variant.alts != None and variant.info ['DP'] != 0:
+                Assembly_info['mismatches'] += 1
+        previous = variant.pos
+else:
+    for variant in variants:
         if variant.pos != previous:
-            Assembly_info['number_of_lines'] += 1
+                Assembly_info['number_of_lines'] += 1
         #Get the number of matches
         if variant.alts == None and variant.info['DP'] != 0:
             Assembly_info['matches'] += 1
         if variant.alts != None and variant.info ['DP'] != 0:
             Assembly_info['mismatches'] += 1
     previous = variant.pos
-
 
 print(Assembly_info)
 
